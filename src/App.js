35efeecd7ec28edcac8cc48085ee9306d90a92b2
Zach Dettwiler <z.dettwiler@gmail.com>
2020-06-08 18:18:40 +0100

    less buggy verse selection

--- src/App.js
+++ src/App.js
@@ -12,48 +12,65 @@ function App() {
   let [chapter, setChapter] = useState(1);
   let [verse, setVerse] = useState(1);
   let [words, setWords] = useState(null);
+  let [bookData, setBookData] = useState([])
   let [verseNumbers, setVerseNumbers] = useState({})
   
 
   async function getWords(newBook, newChapter, newVerse) {
-    // TODO: check if we've already downloaded the book data before doing it again!
-
-    let [words, numbers] = await getBookData(book);
-    setVerseNumbers(numbers);
-    setBook(newBook);
-    setChapter(newBook !== book ? 1 : newChapter);
-    setVerse(newChapter !== chapter ? 1 : newVerse);
-    setWords(words.filter(word => word.chapter === newChapter && word.verse === newVerse));
-
-    console.log(Object.keys(verseNumbers))
+    // get data
+    if (newBook !== book || !bookData.length) {
+      let data = await getBookData(newBook); // TODO return object?
+      setBook(newBook);
+      setBookData(data[0]); 
+      setVerseNumbers(data[1]);
+      setNewReference(data, 1, 1);
+    } else {
+      setNewReference([bookData, verseNumbers], newChapter, newVerse);
+    }    
   }
+
+  function setNewReference(data, newChapter, newVerse) {
+    // set chapter
+    let checkedChapter = Object.keys(data[1]).includes(String(newChapter)) ? newChapter : 1;
+    setChapter(checkedChapter);
+
+    // set verse
+    let checkedVerse = data[1][checkedChapter] && data[1][checkedChapter].includes(newVerse) ? newVerse : 1;
+    setVerse(checkedVerse);
+    
+    // set verse words
+    setWords(data[0].filter(word => word.chapter === checkedChapter && word.verse === checkedVerse));
+  }
+
   useEffect(() => {
     if (words === null) {
       getWords(book, chapter, verse);
     }
   });
 
-  return Object.keys(verseNumbers).length && verseNumbers[chapter] && (
-    <Container className='App'>
-      <VerseSelector
-        book={book}
-        bookOptions={bookOptions}
-        chapter={chapter}
-        chapterOptions={Object.keys(verseNumbers).map(c => ({ text: parseInt(c), value: parseInt(c) }))}
-        verse={verse}
-        verseOptions={verseNumbers[chapter].map(v => ({ text: v, value: v }))}
-        getVerse={getWords}
-      ></VerseSelector>
+  return Object.keys(verseNumbers).length && verseNumbers[chapter]
+    ? (
+      <Container className='App'>
+        <VerseSelector
+          book={book}
+          bookOptions={bookOptions}
+          chapter={chapter}
+          chapterOptions={Object.keys(verseNumbers).map(c => ({ text: parseInt(c), value: parseInt(c) }))}
+          verse={verse}
+          verseOptions={verseNumbers[chapter].map(v => ({ text: v, value: v }))}
+          getVerse={getWords}
+        ></VerseSelector>
 
-      <h1>{book} {chapter}:{verse}</h1>
+        <h1>{book} {chapter}:{verse}</h1>
 
-      { words && words.map(word => (
-        <Word
-          word={word}
-        ></Word>
-      )) }
-    </Container>
-  );
+        { words && words.map(word => (
+          <Word
+            word={word}
+          ></Word>
+        )) }
+      </Container>
+    )
+    : null;
 }
 
 export default App;
