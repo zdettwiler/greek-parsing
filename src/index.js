2819171a1aa899e70f912a83d6df71c4606e3815
Zach Dettwiler <z.dettwiler@gmail.com>
2021-02-06 15:55:05 +0000

    back to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-  BrowserRouter as Router,
-  // HashRouter as Router,
+  // BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
